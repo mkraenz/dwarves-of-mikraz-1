cb6515f56160adf5ef5cf52eceebefa51ce3b6d9
Mirco Kraenz <software@kraenz.eu>
2023-10-27 20:34:49 +0200

    refactor: prep action radius

--- player/ActionRadius.gd
+++ player/ActionRadius.gd
@@ -3,11 +3,10 @@ extends Area2D
 var entered_bodies: Array = []
 
 
-func _on_body_exited(body: Node2D):
-	if not body in entered_bodies:
-		print(body)
+func _on_body_entered(body: Node2D):
+	if body is PhysicsBody2D and body not in entered_bodies:
 		entered_bodies.append(body)
 
 
-func _on_body_entered(body: Node2D):
+func _on_body_exited(body: Node2D):
 	entered_bodies.erase(body)
