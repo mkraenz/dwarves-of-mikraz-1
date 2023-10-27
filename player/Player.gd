757edd99a4e316d57ba6820a1633022e329c1098
Mirco Kraenz <software@kraenz.eu>
2023-10-27 22:52:00 +0200

    feat: player sprite + anims

--- player/Player.gd
+++ player/Player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 class_name Player
 
-@export var speed = 200
+@export var speed = 150
 
 @onready var gstate := GState
 @onready var audio_anims := $AnimationPlayer
+@onready var anim_tree: AnimationTree = $AnimationTree
+
+
+func _ready():
+	anim_tree.active = true
 
 
 func get_input() -> void:
@@ -15,3 +20,24 @@ func get_input() -> void:
 func _physics_process(_delta) -> void:
 	get_input()
 	move_and_slide()
+	update_anim_params()
+
+
+func update_anim_params() -> void:
+	if velocity.x != 0:
+		var dir = velocity.normalized()
+		anim_tree["parameters/attack/blend_position"] = dir.x
+		anim_tree["parameters/idle/blend_position"] = dir.x
+		anim_tree["parameters/move/blend_position"] = dir.x
+
+	if velocity == Vector2.ZERO:
+		anim_tree["parameters/conditions/idle"] = true
+		anim_tree["parameters/conditions/is_moving"] = false
+	else:
+		anim_tree["parameters/conditions/idle"] = false
+		anim_tree["parameters/conditions/is_moving"] = true
+
+	if Input.is_action_pressed("act"):
+		anim_tree["parameters/conditions/attack"] = true
+	else:
+		anim_tree["parameters/conditions/attack"] = false
---
+++ third-party/Elthen/license.md
@@ -0,0 +1,3 @@
+From <https://elthen.itch.io/pixel-art-dwarf-sprites>
+
+License: [Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)](https://creativecommons.org/licenses/by-nc/4.0/) with [additional details here](https://www.patreon.com/posts/27430241)
