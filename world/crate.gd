5bdcaadf7268d31b1cf963f22a41b06ddcdd565a
Mirco Kraenz <software@kraenz.eu>
2023-10-27 23:53:10 +0200

    feat: flash on damage

--- world/crate.gd
+++ world/crate.gd
@@ -1,11 +1,17 @@
 extends RigidBody2D
 
 @onready var stats: Stats = $Stats
+@onready var anims: AnimationPlayer = $AnimationPlayer
 
 
 func _ready():
 	stats.connect("no_health", die)
+	stats.connect("hp_changed", bounce)
 
 
 func die() -> void:
 	queue_free()
+
+
+func bounce(_val) -> void:
+	anims.play("bounce")
