b93726e74177e5be5fbfaae72dacb4518a99c076
Mirco Kraenz <software@kraenz.eu>
2023-10-28 09:05:50 +0200

    feat: align on_damage anim with player attack

--- world/MouseCollider.gd
+++ world/MouseCollider.gd
@@ -1,31 +1,40 @@
 extends Area2D
 
 @onready var cooldown: Timer = $ClickCooldown
+@onready var player_hit_timer: Timer = $PlayerAnimHitTimer
 
-var TIMEOUT = 0.6  # identical to animation time of attack
+var TIMEOUT = 0.6  # identical to animation time of Player attack
+var TIMEOUT_TO_CONNECT = 0.2  # identical to time of Player attack animation when weapon connects with object
 
 
 func _ready():
 	cooldown.one_shot = true
-	cooldown.timeout.connect(on_tick)
+	player_hit_timer.one_shot = true
+	cooldown.timeout.connect(on_cooldown_tick)
+	player_hit_timer.timeout.connect(on_hit_connect)
 
 
 func _input_event(_viewport, _event, _shape_idx):
 	if Input.is_action_pressed("act"):
-		mine()
+		if cooldown.is_stopped():
+			player_hit_timer.start(TIMEOUT_TO_CONNECT)
+			cooldown.start(TIMEOUT)
 
 
 func mine() -> void:
-	if cooldown.is_stopped():
-		var parent_stats = get_node("../Stats")
-		assert(
-			parent_stats,
-			"MouseCollider's parent node must have a Stats node as sibling of MouseCollider"
-		)
-		parent_stats.hp -= 1
+	var parent_stats = get_node("../Stats")
+	assert(
+		parent_stats,
+		"MouseCollider's parent node must have a Stats node as sibling of MouseCollider"
+	)
+	parent_stats.hp -= 1
+
+
+func on_cooldown_tick() -> void:
+	if mouse_entered and Input.is_action_pressed("act"):
+		player_hit_timer.start(TIMEOUT_TO_CONNECT)
 		cooldown.start(TIMEOUT)
 
 
-func on_tick() -> void:
-	if mouse_entered and Input.is_action_pressed("act"):
-		mine()
+func on_hit_connect() -> void:
+	mine()
