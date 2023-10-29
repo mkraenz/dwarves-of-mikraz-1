970745d12c05b4efb5884c465abf05e252bead46
Mirco Kraenz <software@kraenz.eu>
2023-10-29 09:22:24 +0100

    fix: player anim switching between both attack dirs

--- player/Player.gd
+++ player/Player.gd
@@ -7,6 +7,8 @@ class_name Player
 @onready var audio_anims := $AnimationPlayer
 @onready var anim_tree: AnimationTree = $AnimationTree
 
+var lock_animation = false
+
 
 func _ready():
 	anim_tree.active = true
@@ -25,9 +27,10 @@ func _physics_process(_delta) -> void:
 
 func update_anim_params() -> void:
 	var rel_mouse_pos = get_global_mouse_position() - global_position
-	anim_tree["parameters/attack/blend_position"] = rel_mouse_pos.x
 	anim_tree["parameters/idle/blend_position"] = rel_mouse_pos.x
 	anim_tree["parameters/move/blend_position"] = rel_mouse_pos.x
+	if not lock_animation:  # without this lock, when attacking and wiggling with the mouse around the player from left to right, the attack wiggles between attack_left and attack_right, never actually swinging the axe
+		anim_tree["parameters/attack/blend_position"] = rel_mouse_pos.x
 
 	if velocity == Vector2.ZERO:
 		anim_tree["parameters/conditions/idle"] = true
@@ -40,3 +43,11 @@ func update_anim_params() -> void:
 		anim_tree["parameters/conditions/attack"] = true
 	else:
 		anim_tree["parameters/conditions/attack"] = false
+
+
+func _locked_anim_started() -> void:
+	lock_animation = true
+
+
+func _locked_anim_finished() -> void:
+	lock_animation = false
