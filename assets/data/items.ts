a251be84cd751b21badef67539f864e8181686ce
Mirco Kraenz <software@kraenz.eu>
2023-11-12 09:36:00 +0100

    feat: redesign inventory

--- assets/data/items.ts
+++ assets/data/items.ts
@@ -73,4 +73,164 @@ export const itemsData = {
       res_path: "res://third-party/henry_lazarini/I_Coal.png",
     },
   },
+  coal1: {
+    label: "Coal",
+    id: "coal1",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal2: {
+    label: "Coal",
+    id: "coal2",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal3: {
+    label: "Coal",
+    id: "coal3",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal4: {
+    label: "Coal",
+    id: "coal4",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal5: {
+    label: "Coal",
+    id: "coal5",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal6: {
+    label: "Coal",
+    id: "coal6",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal7: {
+    label: "Coal",
+    id: "coal7",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal8: {
+    label: "Coal",
+    id: "coal8",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal9: {
+    label: "Coal",
+    id: "coal9",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal10: {
+    label: "Coal",
+    id: "coal10",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal11: {
+    label: "Coal",
+    id: "coal11",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal12: {
+    label: "Coal",
+    id: "coal12",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal13: {
+    label: "Coal",
+    id: "coal13",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal14: {
+    label: "Coal",
+    id: "coal14",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal15: {
+    label: "Coal",
+    id: "coal15",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal16: {
+    label: "Coal",
+    id: "coal16",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal17: {
+    label: "Coal",
+    id: "coal17",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal18: {
+    label: "Coal",
+    id: "coal18",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal19: {
+    label: "Coal",
+    id: "coal19",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
+  coal20: {
+    label: "Coal",
+    id: "coal20",
+    icon: {
+      type: "Texture2D",
+      res_path: "res://third-party/henry_lazarini/I_Coal.png",
+    },
+  },
 } satisfies ItemData;
--- ui/inventory/inventory_menu.gd
+++ ui/inventory/inventory_menu.gd
@@ -4,7 +4,7 @@ var ginventory := GInventory
 var gdata := GData
 var gstate := GState
 
-@onready var item_list = $M/ScrollContainer/ItemList
+@onready var item_list: ItemList = $M/P/H/ItemList
 
 
 func _physics_process(_delta: float) -> void:
@@ -12,12 +12,28 @@ func _physics_process(_delta: float) -> void:
 		redraw_data()
 
 
-func redraw_data() -> void:
-	Utils.remove_all_children(item_list)
+func _ready():
+	fill_item_list()
 
-	for item_key in ginventory.inventory.keys():
+
+func redraw_data() -> void:
+	# Godot's ItemLists are so ugly to use... they only allow access via index.
+	# So we have to make sure that ginventory.inventory's items and the item list stay in the same order. Not great
+	var i := 0
+	for item_key in ginventory.inventory:
 		var inventory_item = ginventory.inventory[item_key]
-		var item = gdata.items[item_key]
-		var label = Label.new()
-		label.text = "%s : %s" % [item.label, inventory_item.amount]
-		item_list.add_child(label)
+		item_list.set_item_text(i, str(inventory_item.amount))
+		i += 1
+
+
+func fill_item_list() -> void:
+	# Godot's ItemLists are so ugly to use... they only allow access via index.
+	# So we have to make sure that ginventory.inventory's items and the item list stay in the same order. Not great
+	item_list.clear()
+	for item_key in ginventory.inventory:
+		var inventory_item = ginventory.inventory[item_key]
+		var item := gdata.get_item(item_key)
+		var icon = gdata.get_item_icon(item_key)
+		var i = item_list.add_item(str(inventory_item.amount), icon)
+		item_list.set_item_metadata(i, item_key)
+		item_list.set_item_tooltip(i, item.label)
