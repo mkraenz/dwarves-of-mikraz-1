978fa17e1d0ac52cab9495b911bd2705729789d9
Mirco Kraenz <software@kraenz.eu>
2023-11-15 22:27:36 +0100

    ci: print itch.io url to terminal on deploy

--- shipit.sh
+++ shipit.sh
@@ -1,3 +1,7 @@
+# TODO fail pipeline on error of any command
+
 rm -r .build && mkdir .build
 /home/mirco/programming/gamedev/godot4/Godot_v4.1.2-stable_linux.x86_64 --export-release Web .build/index.html
 /home/mirco/programming/gamedev/itchio-butler/butler push .build mkraenz/dwarves-of-mikraz:web
+
+echo 'Deployed successfully. Check itch.io at https://mkraenz.itch.io/dwarves-of-mikraz'
