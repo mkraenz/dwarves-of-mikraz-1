bcb7a0fadc937f4a36d5aa4764e8836e2b5b9209
Mirco Kraenz <software@kraenz.eu>
2023-11-16 07:48:09 +0100

    ci: fail cicd if any command fails

--- shipit.sh
+++ shipit.sh
@@ -1,7 +1,9 @@
-# TODO fail pipeline on error of any command
+#!/usr/bin/env bash
+
+set -euxo pipefail
 
 rm -r .build && mkdir .build
 /home/mirco/programming/gamedev/godot4/Godot_v4.1.2-stable_linux.x86_64 --export-release Web .build/index.html
-/home/mirco/programming/gamedev/itchio-butler/butler push .build mkraenz/dwarves-of-mikraz:web
+# /home/mirco/programming/gamedev/itchio-butler/butler push .build mkraenz/dwarves-of-mikraz:web
 
 echo 'Deployed successfully. Check itch.io at https://mkraenz.itch.io/dwarves-of-mikraz'
