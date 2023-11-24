a6a38aa04f0c83db54f41c2552ba716197d3ef57
Mirco Kraenz <software@kraenz.eu>
2023-11-24 20:03:04 +0100

    refactor: prep for quests

---
+++ assets/data/quests.ts
@@ -0,0 +1,49 @@
+import type { NeededItem } from "./crafting-recipes";
+
+type SuccessCondition = {
+  type: "collect items";
+  items: NeededItem[];
+};
+
+export type Quest = {
+  trigger: { type: "manual" };
+  on_complete: {
+    signal: "win_the_game";
+    signal_params: {};
+  };
+  /** shown in the HUD preview. */
+  teaser: string;
+  /** shown in the quest log. */
+  title: string;
+  /** flavor text shown in the quest log. */
+  description: string;
+  success_conditions: SuccessCondition[];
+};
+
+export type Quests = Record<string, Quest>;
+
+export const quests = {
+  main1: {
+    trigger: {
+      type: "manual",
+    },
+    on_complete: {
+      signal: "win_the_game",
+      signal_params: {},
+    },
+    teaser: "Craft 20 Battle Axes to equip the King's army.",
+    title: "To arms!",
+    description: "flavor text here",
+    success_conditions: [
+      {
+        type: "collect items",
+        items: [
+          {
+            id: "battle_axe",
+            amount: 20,
+          },
+        ],
+      },
+    ],
+  },
+} satisfies Quests;
--- scripts/generate-data-jsons.ts
+++ scripts/generate-data-jsons.ts
@@ -3,6 +3,7 @@ import { itemsData } from "../assets/data/items.ts";
 import { buildingData } from "../assets/data/buildings.ts";
 import { scriptRegistry } from "../assets/data/script.registry.ts";
 import { resourceNodes } from "../assets/data/resource-nodes.ts";
+import { quests } from "../assets/data/quests.ts";
 
 const writeJson = (filepath: string, obj: unknown) => {
   const encoder = new TextEncoder();
@@ -15,3 +16,4 @@ writeJson("./assets/data/items.json", itemsData);
 writeJson("./assets/data/buildings.json", buildingData);
 writeJson("./assets/data/script.registry.json", scriptRegistry);
 writeJson("./assets/data/resource-nodes.json", resourceNodes);
+writeJson("./assets/data/quests.json", quests);
