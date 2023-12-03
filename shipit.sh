ebe66d31ddde0fa8e2f10df29bb17043ec36979f
Mirco Kraenz <software@kraenz.eu>
2023-12-03 11:59:28 +0100

    chore: echo needs -e to enable ansi color codes in scripts

--- shipit.sh
+++ shipit.sh
@@ -19,9 +19,9 @@ godot --export-release Web .build/index.html
 if [ ENVIRONMENT = "prod" ]; then
     echo "Env: prod"
     butler push .build mkraenz/mikraz-blacksmith:web --userversion $(jq -r .version package.json)
-    echo '\033[42mDeployed successfully to production. Check https://mkraenz.itch.io/mikraz-blacksmith'
+    echo -e '\033[42mDeployed successfully to production. Check https://mkraenz.itch.io/mikraz-blacksmith'
 else
     echo "Env: dev"
     butler push .build mkraenz/dwarves-of-mikraz:web --userversion $(jq -r .version package.json)
-    echo '\033[42mDeployed successfully to dev. Check https://mkraenz.itch.io/dwarves-of-mikraz'
+    echo -e '\033[42mDeployed successfully to dev. Check https://mkraenz.itch.io/dwarves-of-mikraz'
 fi
